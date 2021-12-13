9a0ae4eb20782113606a63dd3b8a2a355c150dd9
Håkon Kluasen <hakon.klausen@icloud.com>
2021-12-13 15:44:00 +0100

    Fixed DA table Added consumption table Added draft queries

--- database/ddl/dayahead.sql
+++ database/ddl/dayahead.sql
@@ -1,1 +1,7 @@
-create table dayahead (pricedate date, period int, price decimal(10,2));
+create table dayahead (pricearea char(3), pricedate date, period int, price decimal(10,2));
+
+create unique index i01_dayahead on dayahead(pricearea, pricedate, period);
+
+grant select, insert, update, delete on kratosdb.* to 'kratos_writer'@'localhost';
+
+grant select on kratosdb.* to 'kratos_reader'@'localhost';
