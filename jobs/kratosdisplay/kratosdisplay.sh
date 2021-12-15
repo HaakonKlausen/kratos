d3fef98f596a1fc97575e6a31f6619cecdff6191
Håkon Klausen <hakon.klausen@icloid.com>
2021-12-15 17:16:59 +0100

    Larger font for active power loop i start script for enkel omstart

--- jobs/kratosdisplay/kratosdisplay.sh
+++ jobs/kratosdisplay/kratosdisplay.sh
@@ -3,11 +3,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${DIR}/../common.sh
 
-writeKratosLog "INFO" "Starting Kratosdisplay"
+
 
 # Disable Screensave
 xset s 0
 xset -dpms
 
 # Start display
-python3 ${PYTHONDIR}/kratosdisplay.py
+while true
+do
+	writeKratosLog "INFO" "Starting Kratosdisplay"
+	python3 ${PYTHONDIR}/kratosdisplay.py
+	writeKratosLog "WARN" "Kratosdisplay exited, restarting"
+done
