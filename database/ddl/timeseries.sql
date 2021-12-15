f8ea6cf564e255ac341826e2ad81f17df42df443
Håkon Kluasen <hakon.klausen@icloud.com>
2021-12-15 16:09:57 +0100

    Extended columnt

--- database/ddl/timeseries.sql
+++ database/ddl/timeseries.sql
@@ -1,3 +1,3 @@
-create table timeseries (seriesname char(20), created datetime, value float);
+create table timeseries (seriesname char(30), created datetime, value float);
 
 create unique index i01_timeseries on timeseries(seriesname, created);
