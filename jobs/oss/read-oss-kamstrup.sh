20f53af051aaaa7b43fbd1ea4a21e7145e403f8c
Håkon Kluasen <hakon.klausen@icloud.com>
2021-12-15 08:39:54 +0100

    Added code to read current active power from oss brikken connected to Kamstrup meters

---
+++ jobs/oss/read-oss-kamstrup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${DIR}/../common.sh
+
+# Store PID
+echo $BASHPID >${CONFIG}/read-oss-kamstrup.pid 
+
+# Restart in case it stops
+while true
+do
+	writeKratosLog "INFO" "Starting Read Oss-Brikken app"
+	python3 ${PYTHONDIR}/read_oss_kamstrup.py >>${LOG}
+	writeKratosLog "WARN" "Read Oss-Brikken app aborted"
+done
---
+++ kratos/read_oss_kamstrup.py
@@ -0,0 +1,56 @@
+import serial
+import base64
+import time
+import binascii
+
+import kratoslib
+
+global data_raw
+
+def parse_data(start_pos, length):
+	data = ''
+	for j in range (0, length):
+		data = data + ' ' + str(int(data_raw[start_pos + j]))
+	return data
+
+def readUIntBE(start_pos, length):
+	value=0
+	factor=1
+	for i in range (0, length):
+		value=value+(int(data_raw[start_pos + length - 1 - i]) * factor)
+		factor = factor * 256
+	return value
+
+def parse_message(start_pos):
+	#print('Parsing from ' + str(start_pos))
+	message=''
+	for i in range (0, 6):
+		message = message + '.' + str(data_raw[start_pos + i])
+	subtype = str(data_raw[start_pos + 7])
+	if message == '.1.1.1.7.0.255':
+		# print('Active Power+', readUIntBE(start_pos+7, 4))
+		kratoslib.writeKratosData('oss.active_power', str(readUIntBE(start_pos+7, 4)))
+
+
+ser = serial.Serial('/dev/ttyUSB0', timeout=None, baudrate=115000, xonxoff=False, rtscts=False, dsrdtr=False)
+ser.flushInput()
+ser.flushOutput()
+messages = 0
+while True: 
+	messages = messages + 1
+	try:
+		bytesToRead = ser.inWaiting()
+		if bytesToRead > 0:
+			# print('bytes to read: ' + str(bytesToRead))
+			data_raw = ser.read(bytesToRead)
+			if bytesToRead > 2:
+				for i in range (0, bytesToRead - 9):
+					#print(str(int(data_raw[i])))
+					parse_message(i)
+		time.sleep(1)
+
+	except Exception as e:
+		print('error in reading: ' + str(e))
+		pass
+
+  
