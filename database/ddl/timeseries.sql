75b27702db9302f02466dc9c3e09fc271e109ad4
Håkon Kluasen <hakon.klausen@icloud.com>
2022-08-03 22:05:27 +0200

    Extended seriesname column

--- database/ddl/timeseries.sql
+++ database/ddl/timeseries.sql
@@ -1,3 +1,5 @@
-create table timeseries (seriesname char(30), created datetime, value float);
+create table timeseries (seriesname char(40), created datetime, value float);
 
 create unique index i01_timeseries on timeseries(seriesname, created);
+
+alter table timeseries modify column seriesname char(40);
