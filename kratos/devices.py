a822c8a55d0d8240e13438b49f8c127d0678109a
Håkon Kluasen <hakon.klausen@icloud.com>
2022-12-29 21:24:33 +0100

    Using ovn kjøkken

--- kratos/devices.py
+++ kratos/devices.py
@@ -85,15 +85,15 @@ class CottageOvnerstueDevice:
 
 	def set_power(self, power):
 		if power == constants.Power.On:
-			self.__telldus_api.turnOn(constants.BjonntjonnOvnBad)
-			#self.__telldus_api.turnOn(constants.BjonntjonnOvnKjokken)
-			self.__telldus_api.turnOn(constants.BjonntjonnOvnPeis)
+			#self.__telldus_api.turnOn(constants.BjonntjonnOvnBad)
+			self.__telldus_api.turnOn(constants.BjonntjonnOvnKjokken)
+			#self.__telldus_api.turnOn(constants.BjonntjonnOvnPeis)
 			kratoslib.writeStatuslogData('Bjonntjonn_Ovner', 'On')
 			kratoslib.writeTimeseriesData('bjonntjonn.ovner','1')
 		else:
-			self.__telldus_api.turnOff(constants.BjonntjonnOvnBad)
-			#self.__telldus_api.turnOff(constants.BjonntjonnOvnKjokken)
-			self.__telldus_api.turnOff(constants.BjonntjonnOvnPeis)
+			#self.__telldus_api.turnOff(constants.BjonntjonnOvnBad)
+			self.__telldus_api.turnOff(constants.BjonntjonnOvnKjokken)
+			#self.__telldus_api.turnOff(constants.BjonntjonnOvnPeis)
 			kratoslib.writeStatuslogData('Bjonntjonn_Ovner', 'Off')
 			kratoslib.writeTimeseriesData('bjonntjonn.ovner','0')
 
--- kratos/optimize_algo.py
+++ kratos/optimize_algo.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     optimizer = OptimizeDevice(device=device, numberOfHours=24, numberOfMinutesEachHour=60)
     optimizer.setPower(currentTemperature=float(device.get_temperature()), devicename='Bjønntjønn Varmekabel Inntaksrør', frost_override=frost_override)
 
-    device =  devices.CottageOvnerstueDevice()
+    device =  devices.CottageOvnerstueDevice() 
     optimizer = OptimizeDevice(device=device, numberOfHours=0, numberOfMinutesEachHour=60, minimumTemperature=3.5, maximumTemperature=6.0)
     optimizer.setPower(currentTemperature=float(device.get_temperature()), devicename='Bjønntjønn Ovner Stue', frost_override=frost_override)
 
