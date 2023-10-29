22384bfa4b3bfe43ac3a0c6089acfce3a765499e
Håkon Kluasen <hakon.klausen@icloud.com>
2023-10-29 17:34:38 +0100

    Removed debug

--- kratos/collect_heatpump_power.py
+++ kratos/collect_heatpump_power.py
@@ -14,7 +14,6 @@ class CollectHeatpumpPower:
     def collect_date(self, date):
         datestr = self.dateToStr(date)
         history=self.__panasonic.get_hourly_power_consumption(datestr)
-        print(f"DEBUG: {history}")
         for hour_consumption in history:
             hour=hour_consumption['hour']
             consumpition=hour_consumption['consumption']
